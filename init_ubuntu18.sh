f872e24866f18f6db8bf1023ee703907d9332b55
Kazuhisa MANABE <manakzh@gmail.com>
2019-03-07 06:42:45 +0900

    for ubuntu18

--- init_ubuntu18.sh
+++ init_ubuntu18.sh
@@ -2,6 +2,9 @@
 
 sudo apt install -y openssh-server
 
+sudo perl -p -i.bak -e 's%https?://(?!security)[^ \t]+%http://jp.archive.ubuntu.com/ubuntu/%g' /etc/apt/sources.list
+
+
 sudo timedatectl set-timezone 'Asia/Tokyo'
 
 sudo apt install -y language-pack-ja
@@ -10,7 +13,20 @@ sudo localectl set-locale 'LANG=ja_JP.utf8'
 
 sudo apt install -y manpages-ja manpages-ja-dev
 
+# ホスト名を変更
+# vi /etc/cloud/cloud.cfg
+# preserve_hostname: true
+# hostnamectl set-hostname linux3
+
+# sources.listも更新しないと、色々なパッケージがインストールできない
+
+# network変更
+# vi /etc/netplan/50-cloud-init.yaml 
+# sudo netplan apply
+
+# virtualbox guest addtional
 # apt install gcc make  
 # mount /dev/sr0 /mnt
 # /mnt/VBoxLinuxAdditions.run 
 # reboot
+
--- install_rbenv.sh
+++ install_rbenv.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 APT_PKG="autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev"
+#ubuntu18 APT_PKG="autoconf bison build-essential libssl-dev libyaml-dev libreadline-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm libgdbm-dev"
 YUM_PKG="gcc openssl-devel libyaml-devel libffi-devel readline-devel zlib-devel gdbm-devel ncurses-devel"
 
 if which rbenv > /dev/null; then
