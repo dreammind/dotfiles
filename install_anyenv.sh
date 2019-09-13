86cef221af5d7731fa16bb2dd26350c0d6d36103
Kazuhisa MANABE <manakzh@gmail.com>
2019-09-14 07:17:23 +0900

    Add anyenv-update to the install_anyenv.sh

--- install_anyenv.sh
+++ install_anyenv.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+# for anyenv
 if ! test -d ~/.anyenv; then
   git clone https://github.com/riywo/anyenv ~/.anyenv
 else
   (cd ~/.anyenv; git pull --rebase)
+fi
+
+[ -d ~/.anyenv/plugins ] || mkdir -p ~/.anyenv/plugins
+
+# for anyenv-update
+if test -d ~/.anyenv/plugins/anyenv-update; then
+  (cd ~/.anyenv/plugins/anyenv-update; git pull --rebase)
+else
+  git clone https://github.com/znz/anyenv-update.git ~/.anyenv/plugins/anyenv-update
 fi
 
 if ! grep "anyenv init" ~/.bashrc; then
@@ -11,4 +21,3 @@ if ! grep "anyenv init" ~/.bashrc; then
   echo 'eval "$(anyenv init -)"' >> ~/.bashrc
   echo "Please execute source ~/.bashrc"
 fi
-
