b50b4f0e3108eb6fd7a20c7a516023301b3087b6
Kazuhisa MANABE <kazu@mb-closip.local>
2024-04-10 09:32:27 +0900

    macosの初期の環境整備

---
+++ init_macos.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+brew install --cask deepl
+brew install --cask google-chrome
+brew install --cask visual-studio-code
+brew install --cask slack
+
+# for vim
+brew tap daipeihust/tap && brew install im-select
+brew install autojump
+brew install lv
+brew install telnet
+brew install vim
+brew install wget
